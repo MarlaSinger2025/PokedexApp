178215c446ba98be7e3783dbf3448d49d9c45aac
MarlaSinger2025 <Marla_Singer_2020@web.de>
2025-10-10 14:42:55 +0200

    CreatingForLoop
    Displaying a list of my Pokemon names + height in the browser

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 let pokemonList = [{name:"Nidoqueen" , height:1.3 , type:["ground", "poison"]},
                   {name:"Bulbasaur" , height:0.7 , type:["grass", "poison"]},
                   {name:"Jigglypuff" , height:0.5 , type:["fairy", "normal"]}];
+
+for (let i = 0; i <pokemonList.length; i++)
+    {
+ document.write("<p>" + pokemonList[i].name + " (height:" + pokemonList[i].height + ") </p>");
+}
