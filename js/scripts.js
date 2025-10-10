41d9f354b3e4a915d05394eb5db00f0fc2645fcb
MarlaSinger2025 <Marla_Singer_2020@web.de>
2025-10-10 15:15:13 +0200

    addingComments

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,13 @@ let pokemonList = [{name:"Nidoqueen" , height:1.3 , type:["ground", "poison"]},
 
 for (let i = 0; i <pokemonList.length; i++)
     {
+        //checks if the height of one of the pokemon in the pokemonList is higher than 1
     if (pokemonList[i].height > 1) 
         {
+        //if so, it will write the following string in the browser
     document.write("<p>" + pokemonList[i].name + " (height:" + pokemonList[i].height + ") - Wow, thats BIG!</p>");
 } else {
+        //if not, it will write this string in the browser
     document.write("<p>" + pokemonList[i].name + " (height:" + pokemonList[i].height + ") </p>");
 }
     }
