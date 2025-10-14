88c4aa07e142a33bf6a30c7d3dfdb11b9629be0c
MarlaSinger2025 <Marla_Singer_2020@web.de>
2025-10-14 14:53:22 +0200

    forEachLoop
    changing for into forEach() Loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,7 @@ let pokemonList = [{name:"Nidoqueen" , height:1.3 , type:["ground", "poison"]},
                   {name:"Bulbasaur" , height:0.7 , type:["grass", "poison"]},
                   {name:"Jigglypuff" , height:0.5 , type:["fairy", "normal"]}];
 
-for (let i = 0; i <pokemonList.length; i++)
-    {
-        //checks if the height of one of the pokemon in the pokemonList is higher than 1
-    if (pokemonList[i].height > 1) 
-        {
-        //if so, it will write the following string in the browser
-    document.write("<p>" + pokemonList[i].name + " (height:" + pokemonList[i].height + ") - Wow, thats BIG!</p>");
-} else {
-        //if not, it will write this string in the browser
-    document.write("<p>" + pokemonList[i].name + " (height:" + pokemonList[i].height + ") </p>");
-}
-    }
+
+pokemonList.forEach(function(pokemon) {
+    document.write("<p>" + pokemon.name + " has a height of " + pokemon.height + " an is a " + pokemon.type + " type </p>");
+});
