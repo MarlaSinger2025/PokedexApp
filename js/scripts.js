b0d2680fe12cf9e39c4bf5c8feb4ccd77d361d4a
MarlaSinger2025 <Marla_Singer_2020@web.de>
2025-10-15 12:18:57 +0200

    wrappingPokedexInIIFE
    - adding getALL function and adding another item (pokemon) to the list via push.
    
    - updating forEach() loop to gain access to the pokemonList inside the IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
-let pokemonList = [{name:"Nidoqueen" , height:1.3 , type:["ground", "poison"]},
-                  {name:"Bulbasaur" , height:0.7 , type:["grass", "poison"]},
-                  {name:"Jigglypuff" , height:0.5 , type:["fairy", "normal"]}];
+let pokemonRepository = (function() { //IIFE
+    let pokemonList = [{name:"Nidoqueen" , height:1.3 , type:["ground", "poison"]},
+                       {name:"Bulbasaur" , height:0.7 , type:["grass", "poison"]},
+                       {name:"Jigglypuff" , height:0.5 , type:["fairy", "normal"]}];
 
+return {
 
-pokemonList.forEach(function(pokemon) {
-    document.write("<p>" + pokemon.name + " has a height of " + pokemon.height + " an is a " + pokemon.type + " type </p>");
+    // returns all objects
+    getAll: function() {
+        return pokemonList;
+    },
+
+    // adds an item at the end of the array
+        add: function(item) {
+            pokemonList.push(item);
+        }
+    };
+})();
+
+console.log(pokemonRepository.getAll()); //prints the whole array in the console WITHOUT the added item?
+pokemonRepository.add({name: "Rattata", height: 0.3, type: "normal"}); // specifying which item is gonna be added
+console.log(pokemonRepository.getAll()); //prints the whole array in the console WITH the added item
+
+//accessing pokemonList with the getAll() function because its hidden inside the pokemonRepository
+pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write("<p>" + pokemon.name + " has a height of " + pokemon.height + " and is a " + pokemon.type + " type </p>");
 });
